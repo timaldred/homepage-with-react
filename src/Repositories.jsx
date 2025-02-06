547d070f6073ea5330217a3323cc9f246e77d95b
Tim Aldred <tim5597@gmail.com>
2025-02-06 17:36:46 +0000

    update

--- src/Repositories.jsx
+++ src/Repositories.jsx
@@ -8,27 +8,6 @@ function RepoPreview ( { repo } ) {
       <p>{repo.language} // <a href={repo.html_url}>View on Github</a></p>
       </article>
       </div>
-
-
-
- /*          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-             
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                    <a href="#">
-                      <span className="absolute inset-0" />
-                      {repo.name}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">{repo.description}</p>
-                  <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600">By user #{repo.language}</p>
-                  
-                </div>
-              </article>
-            
-          </div>
-
-          */
     )
   }
 
